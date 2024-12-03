cd2b2746f4c3a98b5edd195b6704620832fe7413
Addison Bartelli <addiebart@ku.edu>
2024-12-02 23:03:15 -0600

    parser p1

---
+++ src/parser.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// takes a lexar-passed string, no errors
+string parse(const string& input) {
+
+    string instr = input;
+
+    // remove spaces
+    for (int i = 0; i < instr.length(); i++) {
+        if (instr[i] == ' ') {
+            instr.erase(i, 1); // remove space
+            i--;
+        }
+    }
+
+    int rightParaI = -1;
+    for (int i = 0; i < instr.length(); i++) {
+        if (instr[i] == ')') {
+            rightParaI = i;
+            break;
+        }
+    } 
+
+    if (rightParaI < 0) {
+        // no preexisting paras remain
+    }
+
+    else {
+        // existing paras remain
+        // find left para
+        int leftParaI;
+        for (int i = rightParaI; i >= 0; i--) {
+            if (instr[i] == '(') {
+                leftParaI = i;
+                break;
+            }
+        }
+
+        string subExp = instr.substr(leftParaI + 1, rightParaI - leftParaI - 1); // get substr inside paras
+        subExp = parse(subExp);
+    }
+}
+
+int main() {
+
+}
