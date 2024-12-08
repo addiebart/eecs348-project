b3630f29ec57e0f687f442777f58e487a99d2663
Addison Bartelli <addiebart@ku.edu>
2024-12-07 18:37:49 -0600

    refined eval

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,10 +1,18 @@
+/*
+EECS 348 Term Project
+Group 3, Fall 2024
+Calculator
+eval.cpp
+*/
+
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <stdexcept>
 
 using namespace std;
 
-bool isNumeric(char c) {
+inline bool isNumeric(char c) {
     switch (c) {
         case '0':
         case '1':
@@ -21,6 +29,7 @@ bool isNumeric(char c) {
     return false;
 }
 
+// performs an operation on two ints stored as strings. returns a string.
 string binaryOperation(const string& left, char op, const string& right) {
     int l = stoi(left);
     int r = stoi(right);
@@ -33,9 +42,17 @@ string binaryOperation(const string& left, char op, const string& right) {
             result = l * r;
             break;
         case '/':
+            // check for division by zero
+            if (r == 0) {
+                throw runtime_error("Division by zero occurred!");
+            }
             result = l / r;
             break;
         case '%':
+            // check for divison by zero
+            if (r == 0) {
+                throw runtime_error("Division by zero occurred!");
+            }
             result = l % r;
             break;
         case '+':
@@ -50,44 +67,92 @@ string binaryOperation(const string& left, char op, const string& right) {
     return to_string(result);
 }
 
-bool minusIsNegation(const string& str, int i) {
-    char leftChar = str[i-1];
-    if (isNumeric(leftChar) || leftChar == ')') {
-        return false;
-    }
-    return true;
+// returns a bool for if a '-' with index i in str is for negation or subtraction based on the character to its left
+// true: negation
+// false: subtraction
+inline bool minusIsNegation(const string& str, int i) {
+    return !(isNumeric(str[i-1]) || str[i-1] == ')');
 }
 
-int fillWithOpsInStr(const string& str, int arr[]) { //fills arr
-    int j = 0;
-    int count = 0;
+// fills arr with indicies of operators in str
+int fillWithOpsInStr(const string& str, int arr[]) {
+    int j = 0; // for array slots
+    int count = 0; // count ops to be able to return arr length to caller
     for (int i = 0; i < str.length(); i++) {
         switch (str[i]) {
-            case '^': // marked p
-            case '*': // marked m
-            case '/': // marked d
-            case '%': // marked r
-            case '+': // marked p
-                arr[j++] = i;
+            case '^':
+            case '*':
+            case '/':
+            case '%':
+            case '+':
+                arr[j++] = i; // use then increment
                 count++;
                 break;
-            case '-':
-                if (minusIsNegation(str, i)) {
-                    break;
-                } else {
+            case '-': 
+                // check if '-' is subtraction or negation
+                if (!minusIsNegation(str, i)) {
                     arr[j++] = i;
                     count++;
-                    break;
                 }
+                break;
         }
     }
-    arr[j] = 0;
     return count;
 }
 
-string multipleOpsBlock(int ops[], int opCount, int priority, const string& str); // proto
+string eval(const string& str); // prototype to be used by multipleOpsBlock
+
+string multipleOpsBlock(int ops[], int opCount, int priority, const string& str) {
+    // cycle indices of operators in ops[]
+    for (int i = 0; i < opCount; i++) {
+        // get the index of the operator in the string
+        int opI = ops[i];
+
+        // determine a condition to select operator(s) based on priority
+        bool condition;
+        switch (priority) {
+            case 1:
+                condition = str[opI] == '^';
+                break;
+            case 2:
+                condition = str[opI] == '/' || str[opI] == '*' || str[opI] == '%';
+                break;
+            case 3:
+                condition = str[opI] == '+' || str[opI] == '-';
+        }
+
+        // if no operators of priority, skip this block
+        if (condition) {
+            // find bounds of substr, inclusive
+            int ssStart = (i == 0) ? 0 : ops[i-1] + 1;
+            int ssEnd = (i == opCount - 1) ? str.length() - 1 : ops[i+1] - 1;
+
+            // find the operands as strings
+            string l = str.substr(ssStart, opI);
+            string r = str.substr(opI + 1, ssEnd - opI); 
+
+            // execute operation
+            string result = binaryOperation(l, str[opI], r);
+            string lwhole = str.substr(0, ssStart);
+            string rwhole = str.substr(ssEnd + 1, str.length() - ssEnd - 1);
+            string newexp = lwhole + result + rwhole;
+
+            // handle dynamically allocated array
+            delete[] ops;
+            ops = nullptr;
+
+            //recurse
+            return eval(newexp);
+        }
+
+    }
+    // signal to caller instance of eval() that no operator of priority exists
+    return "no";
+}
 
 string eval(const string& str) { 
+
+    // find the index of the first ')'
     int rightParaI = -1;
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == ')') {
@@ -96,27 +161,39 @@ string eval(const string& str) {
         }
     } 
 
+    // if no () exists
     if (rightParaI < 0) {
-        // no paras
         int *ops = new int[str.size() + 1];
         int opCount = fillWithOpsInStr(str, ops);
 
-        // no ops - ex: 4 -> 4, sanity case
+        // no ops, ex: 4 -> 4, sanity case
         if (opCount == 0) {
+            // clean dynamically allocated array
             delete[] ops;
             ops = nullptr;
+
+            // return scalar
             return str;
         }
 
+        // one operator, most common base case
         else if (opCount == 1) {
+            // find the index of the operator
             int opI = ops[0];
+
+            // get the operands as strings
             string l = str.substr(0, opI);
             string r = str.substr(opI + 1);
+
+            // clean dynamically allocated array
             delete[] ops;
             ops = nullptr;
+
+            // solve and return
             return binaryOperation(l, str[opI], r);
         }
 
+        // multiple operators, recursive step
         else {
             string blockOut;
 
@@ -136,14 +213,16 @@ string eval(const string& str) {
             blockOut = multipleOpsBlock(ops, opCount, 3, str);
             if (blockOut != "no") {
                 return blockOut;
-            } 
+            }
 
+            // it shouldn't be possible to reach here
             cout << "all blockOuts were no" << endl;
         }
     }
 
+    // () exist
     else {
-        // paras exist
+        // find the corresponding ')'
         int leftParaI;
         for (int i = rightParaI; i >= 0; i--) {
             if (str[i] == '(') {
@@ -152,93 +231,96 @@ string eval(const string& str) {
             }
         }
 
-        string subExp = str.substr(leftParaI + 1, rightParaI - leftParaI - 1); // get substr inside paras
+        // get substr inside ()
+        string subExp = str.substr(leftParaI + 1, rightParaI - leftParaI - 1);
+
+        // recurse on substring
         subExp = eval(subExp);
+
+        // get parts of instr to the left and right of ()
         string leftStr = str.substr(0, leftParaI);
         string rightStr = str.substr(rightParaI + 1);
+
+        // mash strings together to make a new "instr"
         string finalStr = leftStr + subExp + rightStr;
         
+        // recurse on it
         return eval(finalStr);
     }
 
-    return to_string(0xdeadbeef); // should return in an above block, not here.
-    //0xdeadbeef is 3735928559 unsigned dec.
+    // The program shouldn't reach here if input is good from lexar
+    return "Error";
 }
 
-string multipleOpsBlock(int ops[], int opCount, int priority, const string& str) {
-    for (int i = 0; i < opCount; i++) {
-        int opI = ops[i];
-        bool condition;
-        switch (priority) {
-            case 1:
-                condition = str[opI] == '^';
-                break;
-            case 2:
-                condition = str[opI] == '/' || str[opI] == '*' || str[opI] == '%';
-                break;
-            case 3:
-                condition = str[opI] == '+' || str[opI] == '-';
-        }
-        if (condition) {
-            // find start of substr
-            int ssStart;
-            if (i == 0) {
-                ssStart = 0;
-            } else {
-                ssStart = ops[i - 1] + 1;
-            }
-            //find end of substr
-            int ssEnd; // last char, not length of substr
-            if (i == opCount - 1) {
-                ssEnd = str.length() - 1;
-            } else {
-                ssEnd = ops[i + 1] - 1;
-            }
-
-            string l = str.substr(ssStart, opI);
-            string r = str.substr(opI + 1, ssEnd - opI); 
-
-            string result = binaryOperation(l, str[opI], r);
-            string lwhole = str.substr(0, ssStart);
-            string rwhole = str.substr(ssEnd + 1, str.length() - ssEnd - 1);
-            string newexp = lwhole + result + rwhole;
-            delete[] ops;
-            ops = nullptr;
-            return eval(newexp);
-        }
-    }
-    return "no";
-}
-
-inline void test(const string& str, int expected) {
-    cout << "testing \"" + str + "\": ";
+void test(const string& str, int expected) {
+    cout << "Testing \"" + str + "\" = " + to_string(expected) + ": ";
     int actual = stoi(eval(str));
     if (actual == expected) {
         cout << "Passed!" << endl;
     } else {
         cout << "Failed!" << endl;
     }
-    cout << endl;
 }
 
 void testCases() {
+    test("2+2", 4);
+    test("2+2+2", 6);
+    test("2^3", 8);
+    test("-2+5", 3);
+    test("-2-5", -7);
+    test("-2--5", 3);
+    test("6%4/2", 1);
+    test("6%(4/2)", 0);
+    test("((3+5)/2)+(3*5+2)", 21);
+    test("4^-2", 0); // 1/16 = 0 by int divison
     test("-7*(30/1+17%3)-3^2", (-7*((30/1)+(17%3)))-pow(3,2));
+    test("-7*(30/1+17%3)-3^-2", (-7*((30/1)+(17%3)))-pow(3,-2));
+
+    // error cases
+    // div by 0
+    try {
+        cout << "Testing divide by zero: ";
+        eval("10/(6%2)");
+        cout << "Failed!" << endl;
+    } catch (const exception& e) {
+        cout << "Passed!" << endl;
+    }
+    // mod by 0
+    try {
+        cout << "Testing mod by zero: ";
+        eval("10%(6-2*3)");
+        cout << "Failed!" << endl;
+    } catch (const exception& e) {
+        cout << "Passed!" << endl;
+    }
+
 }
 
 int main() {
     while (true) {
+        // prompt and read input
         cout << "Provide an expression to simplify, or enter 't' to run test cases:" << endl;
         string line;
         getline(cin, line);
+
+        // check for and start test cases
         if (line == "t") {
             testCases();
+            cout << endl;
             continue;
         }
-        string result = eval(line);
-        if (stoi(result) == 0xdeadbeef) {
-            cout << "error, got deadbeef" << endl;
-        } else {
-            cout << result << endl;
+        
+        // find result and handle errors
+        string result;
+        try {
+            result = eval(line);
+            if (result == "Error") {
+                cout << "Unknown Error" << endl;
+            } else {
+                cout << result << endl;
+            }
+        } catch (const exception& e) {
+            cout << "Error: " << e.what() << endl;
         }
         cout << endl;
     }
